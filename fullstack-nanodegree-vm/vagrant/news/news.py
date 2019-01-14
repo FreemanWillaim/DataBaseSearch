8e3276da13bef52644aa713064b560bddc3bd400
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-01-14 15:12:36 -0500

    Add variable header5 to hold the percentage of errors header

--- fullstack-nanodegree-vm/vagrant/news/news.py
+++ fullstack-nanodegree-vm/vagrant/news/news.py
@@ -10,3 +10,6 @@ header1 = 'Title'  # A string variable to hold the title header.
 header2 = 'Views'  # A string variable to hold the views header.
 header3 = 'Author'  # A string variable to hold the author header.
 header4 = 'Date'  # A string variable to hold the date header.
+
+
+header5 = 'Percentage of Errors'
