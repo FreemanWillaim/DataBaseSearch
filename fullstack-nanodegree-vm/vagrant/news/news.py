0f0ce660e5686a3531eb8c510c5ec6f7924039d6
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-01-14 15:14:11 -0500

    Add a comment to the variable header5

--- fullstack-nanodegree-vm/vagrant/news/news.py
+++ fullstack-nanodegree-vm/vagrant/news/news.py
@@ -11,5 +11,5 @@ header2 = 'Views'  # A string variable to hold the views header.
 header3 = 'Author'  # A string variable to hold the author header.
 header4 = 'Date'  # A string variable to hold the date header.
 
-
+# A string variable to hold the precentage header.
 header5 = 'Percentage of Errors'
